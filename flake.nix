d6ebeef45d36bd3b921ec0bfa9c064c71cc20f76
fanrongchao <f@xfa.cn>
2025-11-03 03:33:43 +0000

    build: add nix flake for dev shell

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "mihomo-cli development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        rustToolchain = pkgs.rust-bin.stable.latest.default;
+      in {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = [
+            rustToolchain
+            pkgs.pkg-config
+          ];
+          buildInputs = [
+            pkgs.openssl
+            pkgs.curl
+            pkgs.zlib
+          ];
+          shellHook = ''
+            export CARGO_HOME=$PWD/.cargo
+            export RUSTUP_HOME=$PWD/.rustup
+          '';
+        };
+      }
+    );
+}
